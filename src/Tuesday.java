7683baf7b9d8226a09ed1cf0b88af0bc6be485be
kevinmwalugha <kevinmwalugha@gmail.com>
2015-06-30 02:40:06 -0700

    added a divider function

--- src/Tuesday.java
+++ src/Tuesday.java
@@ -3,6 +3,7 @@ public class Tuesday {
 	public static void main(String[] args){
 		printSomething("hello world");
 		computeNums(25,90);
+		divider(90,3);
 	}
 	public static void printSomething(String text){
 		System.out.println(text);
@@ -11,5 +12,9 @@ public class Tuesday {
 	public static void computeNums(int num1, int num2){
 		int sum = num1 + num2;
 		System.out.println("the sum is: " +sum);
+	}
+	public static void divider(int num4, int num3){
+		int total = num4/num3;
+		System.out.println("the total is: " +total);
 	}
 }
